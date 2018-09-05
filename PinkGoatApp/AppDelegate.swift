47f7fd4607fbf0b9de70fa77b0a3fad7de22560e
Roy Li <roy.li.zixuan@gmail.com>
2018-09-04 20:24:33 -0700

    Initial Commit

---
+++ PinkGoatApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PinkGoatApp
+//
+//  Created by Roy Li on 9/4/18.
+//  Copyright © 2018 Roy Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PinkGoatApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PinkGoatApp
+//
+//  Created by Roy Li on 9/4/18.
+//  Copyright © 2018 Roy Li. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
