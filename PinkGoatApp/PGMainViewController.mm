51ac2d8c8dc932bc53d8aa7ea3a509ff4e3b64ec
Roy Li <roy.li.zixuan@gmail.com>
2018-09-16 10:28:11 -0700

    Get code to compile & still need to implement the conversion between Wavefront OBJ to internal data structure

--- PinkGoatApp/PGMainViewController.mm
+++ PinkGoatApp/PGMainViewController.mm
@@ -10,8 +10,25 @@
 #import "BTMultiBodyDynamicsWorldWrapper.h"
 #import "URDF2Bullet.h"
 #import "BulletUrdfImporter.h"
+#import "MyMultiBodyCreator.h"
+#include "BulletDynamics/Featherstone/btMultiBodyDynamicsWorld.h"
+#include "BulletDynamics/Featherstone/btMultiBody.h"
+#import "BulletDynamics/Featherstone/btMultiBodyConstraintSolver.h"
+#include "btDefaultCollisionConfiguration.h"
+#import "bullet/BulletCollision/CollisionDispatch/btCollisionDispatcher.h"
+#import "bullet/BulletCollision/BroadphaseCollision/btOverlappingPairCache.h"
+#import "bullet/BulletCollision/BroadphaseCollision/btDbvtBroadphase.h"
 
-@interface PGMainViewController ()
+@interface PGMainViewController () {
+    btMultiBodyDynamicsWorld* m_dynamicsWorld;
+    btMultiBody* m_multiBody;
+    btAlignedObjectArray<btCollisionShape*>    m_collisionShapes;
+    btDefaultCollisionConfiguration* m_collisionConfiguration;
+    btCollisionDispatcher*    m_dispatcher;
+    btOverlappingPairCache* m_pairCache;
+    btBroadphaseInterface*    m_broadphase;
+    btMultiBodyConstraintSolver*    m_solver;
+}
 
 @end
 
@@ -24,25 +41,25 @@
 
 - (void)viewDidAppear {
     [super viewDidAppear];
+    [self importRobotModel];
 }
 
 // TODO: move this to model layer.
 - (void)importRobotModel {
+    
+    [self createEmptyDynamicsWorld];
+    
     BulletURDFImporter u2b(NULL,0,1,0);
-    bool loadOk = u2b.loadURDF("kuka_iiwa/model.urdf");// lwr / kuka.urdf");
+    
+    bool loadOk = u2b.loadURDF("/Users/royli/Documents/bullet3/data/kuka_iiwa/model.urdf");// lwr / kuka.urdf");
     if (loadOk)
     {
         int rootLinkIndex = u2b.getRootLinkIndex();
-        
-        // TODO: How does p3Printf differ from printf?
-        
-//        b3Printf("urdf root link index = %d\n",rootLinkIndex);
-        
-        // TODO: need to port the MultiBodyCreator class
-        MyMultiBodyCreator creation(m_guiHelper);
+        MyMultiBodyCreator creation;
         btTransform identityTrans;
         identityTrans.setIdentity();
-        ConvertURDF2Bullet(u2b, creation, identityTrans,m_dynamicsWorld,true,u2b.getPathPrefix());
+        
+        ConvertURDF2Bullet(u2b, creation, identityTrans, m_dynamicsWorld, true,u2b.getPathPrefix());
         for (int i = 0; i < u2b.getNumAllocatedCollisionShapes(); i++)
         {
             m_collisionShapes.push_back(u2b.getAllocatedCollisionShape(i));
@@ -54,9 +71,33 @@
             //temporarily set some extreme damping factors until we have some joint control or constraints
             m_multiBody->setAngularDamping(0*0.99);
             m_multiBody->setLinearDamping(0*0.99);
-            b3Printf("Root link name = %s",u2b.getLinkName(u2b.getRootLinkIndex()).c_str());
+            printf("Root link name = %s",u2b.getLinkName(u2b.getRootLinkIndex()).c_str());
         }
     }
+
+}
+
+- (void)createEmptyDynamicsWorld
+{
+    ///collision configuration contains default setup for memory, collision setup
+    m_collisionConfiguration = new btDefaultCollisionConfiguration();
+    //m_collisionConfiguration->setConvexConvexMultipointIterations();
+//    m_filterCallback = new MyOverlapFilterCallback2();
+    
+    ///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+    m_dispatcher = new    btCollisionDispatcher(m_collisionConfiguration);
+    
+    m_pairCache = new btHashedOverlappingPairCache();
+    
+//    m_pairCache->setOverlapFilterCallback(m_filterCallback);
+    
+    m_broadphase = new btDbvtBroadphase(m_pairCache);//btSimpleBroadphase();
+    
+    m_solver = new btMultiBodyConstraintSolver;
+    
+    m_dynamicsWorld = new btMultiBodyDynamicsWorld(m_dispatcher, m_broadphase, m_solver, m_collisionConfiguration);
+    
+    m_dynamicsWorld->setGravity(btVector3(0, -10, 0));
 
 }
 
--- PinkGoatApp/b3ImportMeshUtility.cpp
+++ PinkGoatApp/b3ImportMeshUtility.cpp
@@ -77,87 +77,87 @@ bool b3ImportMeshUtility::loadAndRegisterMeshFromFileInternal(const std::string&
 		}
 		
         printf("Calling commentted out code\n");
-//        GLInstanceGraphicsShape* gfxShape = btgCreateGraphicsShapeFromWavefrontObj(shapes);
-//        {
-//            B3_PROFILE("Load Texture");
-//            //int textureIndex = -1;
-//            //try to load some texture
-//            for (int i = 0; meshData.m_textureImage1 == 0 && i < shapes.size(); i++)
-//            {
-//                const tinyobj::shape_t& shape = shapes[i];
-//                if (shape.material.diffuse_texname.length() > 0)
-//                {
-//
-//                    int width, height, n;
-//                    const char* filename = shape.material.diffuse_texname.c_str();
-//                    unsigned char* image = 0;
-//
-//                    const char* prefix[] = { pathPrefix, "./", "./data/", "../data/", "../../data/", "../../../data/", "../../../../data/" };
-//                    int numprefix = sizeof(prefix) / sizeof(const char*);
-//
-//                    for (int i = 0; !image && i < numprefix; i++)
-//                    {
-//                        char relativeFileName[1024];
-//                        sprintf(relativeFileName, "%s%s", prefix[i], filename);
-//                        char  relativeFileName2[1024];
-//                        if (b3ResourcePath::findResourcePath(relativeFileName, relativeFileName2, 1024))
-//                        {
-//                            if (b3IsFileCachingEnabled())
-//                            {
-//                                CachedTextureResult* texture = gCachedTextureResults[relativeFileName];
-//                                if (texture)
-//                                {
-//                                    image = texture->m_pixels;
-//                                    width = texture->m_width;
-//                                    height = texture->m_height;
-//                                    meshData.m_textureWidth = width;
-//                                    meshData.m_textureHeight = height;
-//                                    meshData.m_textureImage1 = image;
-//                                    meshData.m_isCached = true;
-//                                }
-//                            }
-//
-//                            if (image==0)
-//                            {
-//                                image = stbi_load(relativeFileName, &width, &height, &n, 3);
-//
-//                                meshData.m_textureImage1 = image;
-//
-//                                if (image)
-//                                {
-//                                    meshData.m_textureWidth = width;
-//                                    meshData.m_textureHeight = height;
-//
-//                                    if (b3IsFileCachingEnabled())
-//                                    {
-//                                        CachedTextureResult result;
-//                                        result.m_textureName = relativeFileName;
-//                                        result.m_width = width;
-//                                        result.m_height = height;
-//                                        result.m_pixels = image;
-//                                        meshData.m_isCached = true;
-//                                        gCachedTextureResults.insert(relativeFileName,result);
-//                                    }
-//                                }
-//                                else
-//                                {
-//                                    b3Warning("Unsupported texture image format [%s]\n", relativeFileName);
-//
-//                                    break;
-//                                }
-//                            }
-//
-//                        }
-//                        else
-//                        {
-//                            b3Warning("not found [%s]\n", relativeFileName);
-//                        }
-//                    }
-//                }
-//            }
-//
-//        }
-//        meshData.m_gfxShape = gfxShape;
+        GLInstanceGraphicsShape* gfxShape = btgCreateGraphicsShapeFromWavefrontObj(shapes);
+        {
+            B3_PROFILE("Load Texture");
+            //int textureIndex = -1;
+            //try to load some texture
+            for (int i = 0; meshData.m_textureImage1 == 0 && i < shapes.size(); i++)
+            {
+                const tinyobj::shape_t& shape = shapes[i];
+                if (shape.material.diffuse_texname.length() > 0)
+                {
+
+                    int width, height, n;
+                    const char* filename = shape.material.diffuse_texname.c_str();
+                    unsigned char* image = 0;
+
+                    const char* prefix[] = { pathPrefix, "./", "./data/", "../data/", "../../data/", "../../../data/", "../../../../data/" };
+                    int numprefix = sizeof(prefix) / sizeof(const char*);
+
+                    for (int i = 0; !image && i < numprefix; i++)
+                    {
+                        char relativeFileName[1024];
+                        sprintf(relativeFileName, "%s%s", prefix[i], filename);
+                        char  relativeFileName2[1024];
+                        if (b3ResourcePath::findResourcePath(relativeFileName, relativeFileName2, 1024))
+                        {
+                            if (b3IsFileCachingEnabled())
+                            {
+                                CachedTextureResult* texture = gCachedTextureResults[relativeFileName];
+                                if (texture)
+                                {
+                                    image = texture->m_pixels;
+                                    width = texture->m_width;
+                                    height = texture->m_height;
+                                    meshData.m_textureWidth = width;
+                                    meshData.m_textureHeight = height;
+                                    meshData.m_textureImage1 = image;
+                                    meshData.m_isCached = true;
+                                }
+                            }
+
+                            if (image==0)
+                            {
+                                image = stbi_load(relativeFileName, &width, &height, &n, 3);
+
+                                meshData.m_textureImage1 = image;
+
+                                if (image)
+                                {
+                                    meshData.m_textureWidth = width;
+                                    meshData.m_textureHeight = height;
+
+                                    if (b3IsFileCachingEnabled())
+                                    {
+                                        CachedTextureResult result;
+                                        result.m_textureName = relativeFileName;
+                                        result.m_width = width;
+                                        result.m_height = height;
+                                        result.m_pixels = image;
+                                        meshData.m_isCached = true;
+                                        gCachedTextureResults.insert(relativeFileName,result);
+                                    }
+                                }
+                                else
+                                {
+                                    b3Warning("Unsupported texture image format [%s]\n", relativeFileName);
+
+                                    break;
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            b3Warning("not found [%s]\n", relativeFileName);
+                        }
+                    }
+                }
+            }
+
+        }
+        meshData.m_gfxShape = gfxShape;
 		return true;
 	
 	}
